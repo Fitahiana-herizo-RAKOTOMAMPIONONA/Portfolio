d2d3b96f059c73f49d920a049e9dd85dcfb0f73f
RAKOTOMAMPIONONA <Fitahiana Herizo>
2023-10-28 09:15:29 +0200

    feat : first Commit

--- src/page/experience/experience.jsx
+++ src/page/experience/experience.jsx
@@ -1,4 +1,46 @@
 import { Component } from "react";
+import {FaBriefcase} from "react-icons/fa"
+import {FaGraduationCap} from "react-icons/fa"
+
+function QualificationData({titre,sousTitre,calendrier}){
+    return <div className="qualificationData">
+                <div>
+                    <h3 className="SectionTitre">{titre}</h3>
+                    <span className="qualificationSousTitre">
+                        {sousTitre}
+                    </span>
+                    <div className="qualificationCalendar">
+                        <FaBriefcase/> {"   " + calendrier}
+                    </div>
+                </div>
+
+                <div>
+                    <span className="qualificationRounder">
+
+                    </span>
+                    <div className="qualificationLine">
+
+                    </div>
+                </div>
+            </div>
+}
+const DataExperience =[
+    {
+        titre: "vio",
+        sousTitre:"",
+        calendrier : ""
+    },
+    {
+        titre: "visdso",
+        sousTitre:"",
+        calendrier : ""
+    },
+    {
+        titre: "vio",
+        sousTitre:"",
+        calendrier : ""
+    },
+]
 
 class Experience extends Component{
     render(){
@@ -6,6 +48,40 @@ class Experience extends Component{
             <h1 className="sectionTitre">
                 Experience
             </h1>
+
+            <div className="qualificaionContainer Container">
+                <div className="qualificationHeader">
+                    <div className="sualificationButton">
+                        <FaGraduationCap/>
+                        <span>
+                            education 
+                        </span>
+                    </div>
+                    <div className="sualificationButton">
+                        <FaBriefcase/>
+                        <span>
+                            Experience 
+                        </span>
+                    </div>
+                </div>
+
+                <div className="qualificationSection">
+                    <div className="qualificationContent">
+                        {
+                            DataExperience.map((item,index)=>(
+                                <QualificationData 
+                                    key={index}
+                                    titre={item.titre} 
+                                    sousTitre={item.sousTitre} 
+                                    calendrier={item.calendrier}
+                                />
+
+                            ))
+                        }
+
+                    </div>
+                </div>
+            </div>
         </div>
     }
 }
